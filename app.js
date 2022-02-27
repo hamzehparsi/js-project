6f16c253ae02a49ad194ec9fdf9a9d5d857c7bf2
hamzehparsi <hamzehparsi@gmail.com>
2022-02-28 00:44:10 +0330

    link title

---
+++ app.js
@@ -0,0 +1,30 @@
+let products = getSaveProducts()
+
+const filters = {
+    searchItem: '',
+    availableProducts: false
+}
+
+renderProducts(products, filters)
+
+document.querySelector('#search-products').addEventListener('input', function(e) {
+    filters.searchItem = e.target.value
+    renderProducts(products, filters)
+})
+
+document.querySelector('#add-product-form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    products.push({
+        id: uuidv4(),
+        title: e.target.elements.productTitle.value,
+        exist: true
+    })
+    saveProducts(products)
+    renderProducts(products, filters)
+    e.target.elements.productTitle.value = ''
+})
+
+document.querySelector('#available-products').addEventListener('change', function(e) {
+    filters.availableProducts = e.target.checked
+    renderProducts(products, filters)
+})
